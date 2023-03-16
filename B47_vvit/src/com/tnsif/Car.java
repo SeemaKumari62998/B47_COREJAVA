a8c532b5c3ba6fcbefcb6803050f1fe47a0e926f
seema <seema@DESKTOP-TAOLDBD>
2023-03-16 23:00:25 +0530

    First Commit

---
+++ B47_vvit/src/com/tnsif/Car.java
@@ -0,0 +1,11 @@
+package com.tnsif;
+
+public class Car {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ B47_vvit/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author seema
+ *
+ */
+module B47_vvit {
+}
